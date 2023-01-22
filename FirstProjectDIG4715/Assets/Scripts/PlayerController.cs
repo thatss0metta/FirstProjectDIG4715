e3f70e37225965776126746f07d0f0f7ddd7abe9
thatss0metta <mario.galanreyes@gmail.com>
2023-01-22 02:39:59 -0500

    Movement

--- FirstProjectDIG4715/Assets/Scripts/PlayerController.cs
+++ FirstProjectDIG4715/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rd2d;
+    private float horizontal;
+    private float speed = 8f;
+    private int scoreValue = 0;
+    public Text score;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+      rd2d = GetComponent<Rigidbody2D>();
+      score.text = scoreValue.ToString();  
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
     }
-}
+
+     private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if (collision.collider.tag == "Collectible")
+        {
+            scoreValue += 1;
+            score.text = scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+    }
+}    
